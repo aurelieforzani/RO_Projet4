f19d07d567bafd4bd772f36ad532575ac7fd17ac
Forzani Aurelie <aforzani@localhost.localdomain>
2017-11-30 13:59:48 +0100

    commentaire poisson

--- poisson.m
+++ poisson.m
@@ -1,10 +1,21 @@
 function [x] = poisson(k)
 
+%% EN ENTREE
+% k : nombre de succès
+
+%% EN SORTIE
+% x : probabilité attendue
+
+%% Déclaration de la variables globale
+% Nombre maximal de demandes
 global lambda;
 
+%% Calcul de la probabilité
 if ( k > 0)
+% Si k est dans le domaine de définition
     x = (exp(-lambda)*lambda^(k))/(factorial(k));
 else 
+% sinon la probabilité est nulle
     x=0;
 end
 return;
